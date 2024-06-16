d091879bafd9d1c8a86c2b132c4d058a54b861e9
00yhsp <00yhsp@naver.com>
2024-06-16 13:20:36 +0900

    [#1] WKWebView UIViewRepresentable, 스크립트 주입 구현

---
+++ NC2WebKit/NC2WebKit/WebView.swift
@@ -0,0 +1,47 @@
+//
+//  WebView.swift
+//  NC2WebKit
+//
+//  Created by 박현수 on 6/16/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: String
+    let webView: WKWebView = WKWebView()
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webView.evaluateJavaScript("document.querySelector('#time_area').scrollIntoView({behavior: 'smooth', block: 'start'});")
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: url) else {
+            return WKWebView()
+        }
+        webView.navigationDelegate = context.coordinator
+        webView.load(URLRequest(url: url))
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: UIViewRepresentableContext<WebView>) {
+        guard let url = URL(string: url) else { return }
+        
+        webView.load(URLRequest(url: url))
+    }
+    
+}
